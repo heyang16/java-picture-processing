a95dae71bfad97d250d25bcab38ccffdbd745e17
Heyang <heyang16@users.noreply.github.com>
2023-01-17 16:16:16 +0000

    used switch instead of if

--- src/picture/PictureProcessor.java
+++ src/picture/PictureProcessor.java
@@ -5,51 +5,60 @@ import java.util.List;
 
 public class PictureProcessor {
     public static void main(String[] args) {
-        if (args[0].compareTo("invert") == 0) {
-            Picture input = new Picture(args[1]);
-            input.invert();
-            input.saveAs(args[2]);
-        }
-        if (args[0].compareTo("grayscale") == 0) {
-            Picture input = new Picture(args[1]);
-            input.grayscale();
-            input.saveAs(args[2]);
-        }
-        if (args[0].compareTo("rotate") == 0) {
-            Picture input = new Picture(args[2]);
-            if (args[1].compareTo("90") == 0) {
-                Picture out = input.rotate90();
-                out.saveAs(args[3]);
-            } else if (args[1].compareTo("180") == 0) {
-                Picture out = input.rotate180();
-                out.saveAs(args[3]);
-            } else if (args[1].compareTo("270") == 0) {
-                Picture out = input.rotate270();
-                out.saveAs(args[3]);
+        switch (args[0]) {
+            case "invert" -> {
+                Picture input = new Picture(args[1]);
+                input.invert();
+                input.saveAs(args[2]);
             }
-        }
-        if (args[0].compareTo("flip") == 0) {
-            Picture input = new Picture(args[2]);
-            if (args[1].compareTo("H") == 0) {
-                Picture out = input.flipHorizontal();
-                out.saveAs(args[3]);
-            } else if (args[1].compareTo("V") == 0) {
-                Picture out = input.flipVertical();
-                out.saveAs(args[3]);
+            case "grayscale" -> {
+                Picture input = new Picture(args[1]);
+                input.grayscale();
+                input.saveAs(args[2]);
             }
-        }
-        if (args[0].compareTo("blend") == 0) {
-            List<Picture> inputs = new ArrayList<>();
-            for (int i = 1; i < args.length - 1; i++) {
-                inputs.add(new Picture(args[i]));
+            case "rotate" -> {
+                Picture input = new Picture(args[2]);
+                switch (args[1]) {
+                    case "90" -> {
+                        Picture out = input.rotate90();
+                        out.saveAs(args[3]);
+                    }
+                    case "180" -> {
+                        Picture out = input.rotate180();
+                        out.saveAs(args[3]);
+                    }
+                    case "270" -> {
+                        Picture out = input.rotate270();
+                        out.saveAs(args[3]);
+                    }
+                }
             }
-            Picture out = Picture.blend(inputs);
-            out.saveAs(args[args.length - 1]);
-        }
-        if (args[0].compareTo("blur") == 0) {
-            Picture input = new Picture(args[1]);
-            Picture out = input.blur();
-            out.saveAs(args[2]);
+            case "flip" -> {
+                Picture input = new Picture(args[2]);
+                switch (args[1]) {
+                    case "H" -> {
+                        Picture out = input.flipHorizontal();
+                        out.saveAs(args[3]);
+                    }
+                    case "V" -> {
+                        Picture out = input.flipVertical();
+                        out.saveAs(args[3]);
+                    }
+                }
+            }
+            case "blend" -> {
+                List<Picture> inputs = new ArrayList<>();
+                for (int i = 1; i < args.length - 1; i++) {
+                    inputs.add(new Picture(args[i]));
+                }
+                Picture out = Picture.blend(inputs);
+                out.saveAs(args[args.length - 1]);
+            }
+            case "blur" -> {
+                Picture input = new Picture(args[1]);
+                Picture out = input.blur();
+                out.saveAs(args[2]);
+            }
         }
     }
 }
